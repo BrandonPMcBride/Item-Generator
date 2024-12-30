22bfd1d20e531455b7d1fcd43be86c87c1a1067e
BrandonPMcBride <brandonpmcbride@gmail.com>
2024-12-30 17:52:52 -0600

    finish up base requirements of generation

--- generator.js
+++ generator.js
@@ -1,4 +1,13 @@
-const adjective = [Vivid,Innovative,Dynamic,Serene,Robust,Graceful,Luminous,Resolute,Vibrant,Efficient];
-const noun = [Book,Bicycle,Lamp,Teddy bear,Backpack,Soccer ball,Puzzle,Water bottle,Spoon,Picture frame];
-const color = [Crimson,Azure,Emerald,Amber,Cerulean,Magenta,Ochre,Lavender,Teal,Coral];
-const quote = `You have created a ${adjective} ${color} ${noun}`;
+const adjective = ["Vivid", "Innovative", "Dynamic", "Serene", "Robust", "Graceful", "Luminous", "Resolute", "Vibrant", "Efficient"];
+const noun = ["Book", "Bicycle", "Lamp", "Teddy Bear", "Backpack", "Soccer Ball", "Puzzle", "Water Bottle", "Spoon", "Picture Frame"];
+const color = ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Black", "White", "Gray"];
+
+function generateRandomItem() {
+    const randomIndex = Math.floor(Math.random() * 10);
+    return `You have created a... \n${adjective[randomIndex]} ${color[randomIndex]} ${noun[randomIndex]}!`
+}
+
+document.getElementById("generateButton").addEventListener("click", function() {
+    const itemPrinted = generateRandomItem();
+    document.getElementById("displayVariable").innerText = itemPrinted; 
+});
