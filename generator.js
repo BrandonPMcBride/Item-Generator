804461a2f5e8e1b6b4ba3872e9550d0ff7e1feb8
BrandonPMcBride <brandonpmcbride@gmail.com>
2024-12-30 15:56:36 -0600

    change the theme to item and add pieces

---
+++ generator.js
@@ -0,0 +1,4 @@
+const adjective = [Vivid,Innovative,Dynamic,Serene,Robust,Graceful,Luminous,Resolute,Vibrant,Efficient];
+const noun = [Book,Bicycle,Lamp,Teddy bear,Backpack,Soccer ball,Puzzle,Water bottle,Spoon,Picture frame];
+const color = [Crimson,Azure,Emerald,Amber,Cerulean,Magenta,Ochre,Lavender,Teal,Coral];
+const quote = `You have created a ${adjective} ${color} ${noun}`;
